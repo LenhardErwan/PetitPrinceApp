d66a41a7a47b07f9fbf5e998a45de870b3123700
LenhardErwan <erwan.lenhard@etu.univ-amu.fr>
2020-03-10 15:19:11 +0100

    Update tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'articles',
+    loadChildren: () => import('./articles/articles.module').then( m => m.ArticlesPageModule)
+  },
+  {
+    path: 'dates',
+    loadChildren: () => import('./dates/dates.module').then( m => m.DatesPageModule)
+  },
+  {
+    path: 'galeries',
+    loadChildren: () => import('./galeries/galeries.module').then( m => m.GaleriesPageModule)
   }
 ];
 @NgModule({
