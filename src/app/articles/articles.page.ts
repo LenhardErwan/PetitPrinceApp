3e1c4689ecb5b70f66ccb292665be741f4141e0a
Arnaud <mouton1.9@hotmail.fr>
2020-03-11 11:47:18 +0100

    articles

--- src/app/articles/articles.page.ts
+++ src/app/articles/articles.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-articles',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./articles.page.scss'],
 })
 export class ArticlesPage implements OnInit {
+  reponse: any;
+  photos: any;
+  constructor(private http: HttpClient) { 
+    this.http.get('http://www.sebastien-thon.fr/cours/M4104Cip/projet/index.php?login=classe1&mdp=mdp1')
+      .subscribe((data) => {
+        this.reponse = data;
+        this.reponse = this.reponse.articles;
+        console.log(this.reponse);
+      });
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
 
+
+
+
+
+
 }
+
