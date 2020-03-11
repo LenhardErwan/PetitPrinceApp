3eed68a7ddf027577e69b5872c53d00efc3179a2
Arnaud <mouton1.9@hotmail.fr>
2020-03-11 19:57:34 +0100

    galerie

--- src/app/galeries/galeries.module.ts
+++ src/app/galeries/galeries.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 import { IonicModule } from '@ionic/angular';
 
 import { GaleriesPageRoutingModule } from './galeries-routing.module';
@@ -13,6 +13,7 @@ import { GaleriesPage } from './galeries.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpClientModule,
     GaleriesPageRoutingModule
   ],
   declarations: [GaleriesPage]
--- src/app/galeries/galeries.page.ts
+++ src/app/galeries/galeries.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { AuthGuardService } from '../services/auth-guard.service';
 
 @Component({
@@ -7,8 +8,16 @@ import { AuthGuardService } from '../services/auth-guard.service';
   styleUrls: ['./galeries.page.scss'],
 })
 export class GaleriesPage implements OnInit {
+  private reponse: any;
 
-  constructor(private authGuard: AuthGuardService) { }
+  constructor(private http: HttpClient, private authGuard: AuthGuardService) {
+    this.http.get('http://www.sebastien-thon.fr/cours/M4104Cip/projet/index.php?login=classe1&mdp=mdp1')
+      .subscribe((data) => {
+        this.reponse = data;
+        this.reponse = this.reponse.galeries;
+        console.log(this.reponse);
+      });
+   }
 
   ngOnInit() {
   }
